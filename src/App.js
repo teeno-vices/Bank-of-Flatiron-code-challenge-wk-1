f1ab3397a42bc1692937f9fe971f1b7705bcecea
robemike <robemike1135@gmail.com>
2024-05-08 00:22:33 +0300

    Update application

--- src/App.js
+++ src/App.js
@@ -4,47 +4,8 @@ import Tables from './components/Tables.js'
 import Navbar from './components/Navbar.js';
 import SearchTransaction from './components/SearchTransaction.js'
 import TransactionInformation from './components/TransactionInformation.js';
-
+import { initialTransactions } from './components/InitialTransactions.js';
 function App() {
-
-  const initialTransactions = [
-    {
-      date: new Date('1/12/2019'),
-      description: 'Paycheck from Bob`s Burgers',
-      category: 'Income',
-      amount: 1000
-    },
-    {
-      date: new Date('1/12/2019'),
-      description: 'South by Southwest Quinoa Bowl at Fresh & Co',
-      category: 'Food',
-      amount: -10.55
-    },
-    {
-      date: new Date('2/12/2019'),
-      description: 'South by Southwest Quinoa Bowl at Fresh & Co',
-      category: 'Food',
-      amount: -10.55
-    },
-    {
-      date: new Date('4/12/2019'),
-      description: 'Sunglasses, Urban Outfitters',
-      category: 'Fashion',
-      amount: -24.99
-    },
-    {
-      date: new Date('6/12/2019'),
-      description: 'Venmo, Alice pays you for Burritos',
-      category: 'Food',
-      amount: 8.75
-    },
-    {
-      date: new Date('8/12/2019'),
-      description: 'Chipotle',
-      category: 'Food',
-      amount: -17.59
-    }
-  ];
 
 const [transactions, setTransactions] = useState(initialTransactions);
 const [searchQuery, setSearchQuery] = useState('');
---
+++ src/components/InitialTransactions.js
@@ -0,0 +1,38 @@
+export const initialTransactions = [
+    {
+      date: new Date('1/12/2019'),
+      description: 'Paycheck from Bob`s Burgers',
+      category: 'Income',
+      amount: 1000
+    },
+    {
+      date: new Date('1/12/2019'),
+      description: 'South by Southwest Quinoa Bowl at Fresh & Co',
+      category: 'Food',
+      amount: -10.55
+    },
+    {
+      date: new Date('2/12/2019'),
+      description: 'South by Southwest Quinoa Bowl at Fresh & Co',
+      category: 'Food',
+      amount: -10.55
+    },
+    {
+      date: new Date('4/12/2019'),
+      description: 'Sunglasses, Urban Outfitters',
+      category: 'Fashion',
+      amount: -24.99
+    },
+    {
+      date: new Date('6/12/2019'),
+      description: 'Venmo, Alice pays you for Burritos',
+      category: 'Food',
+      amount: 8.75
+    },
+    {
+      date: new Date('8/12/2019'),
+      description: 'Chipotle',
+      category: 'Food',
+      amount: -17.59
+    }
+  ];
