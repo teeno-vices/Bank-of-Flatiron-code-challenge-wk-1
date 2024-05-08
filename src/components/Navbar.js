fbfc8b5ad40a9e34ce3e5b8fa32fa92b22b3acac
Marc Ndegwa <macndegwa11296@gmail.com>
2024-05-08 18:26:01 +0300

    I have completed this code challenge

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Navbar() {
   return (
     <div>
-      <h1 id='header'>The royal bank of Flatiron</h1> 
+      <h1 id='header'>The Royal Bank of Flatiron</h1> 
     </div>
   )
 }
